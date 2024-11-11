5d36b5487067ce0385a1d107d54c69efbbc59643
uran <168523312+uranv@users.noreply.github.com>
2024-11-12 05:10:43 +0900

    Update Bilibili_remove_ads.js

--- Bilibili_remove_ads.js
+++ Bilibili_remove_ads.js
@@ -54,7 +54,7 @@ if (url.includes("/x/resource/show/tab/v2")) {
         if (item?.style === 1 || item?.style === 2) {
           if (item?.title) {
             if (item?.title === "创作中心") {  // 创作中心
-              if (item?.items?.length > 0 && item.items[5]) {  //保留创作中心/稿件管理/主播中心/直播数据凑成一行
+              if (item?.items?.length > 0 && item.items[7]) {  //保留创作中心/稿件管理/主播中心/直播数据凑成一行
                 item.items = [item.items[0], item.items[1], item.items[5], item.items[6]].filter(Boolean);
                 continue;
               }
