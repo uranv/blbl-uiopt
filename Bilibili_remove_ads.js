62ee3be9d96089fd7cfe440f229f9126ad57ed8c
uran <168523312+uranv@users.noreply.github.com>
2024-11-12 13:46:44 +0900

    Update Bilibili_remove_ads.js

--- Bilibili_remove_ads.js
+++ Bilibili_remove_ads.js
@@ -46,12 +46,7 @@ if (url.includes("/x/resource/show/tab/v2")) {
   // iPhone 我的页面
   if (obj?.data?.sections_v2?.length > 0) {
     let newSects = [];
-    for (let item of obj.data.sections_v2) {
-    
-      const propertyNames = Object.keys(item);  //输出变量名
-      //console.log(propertyNames);  //输出变量名
-      persistentStore.write("prop_sections_v2:",propertyNames)  //输出变量名
-      
+    for (let item of obj.data.sections_v2) {      
       if (item?.button) {
         delete item.button;
       }
